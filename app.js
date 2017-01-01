b0f32dfc933a6e7715cc4954d41e24a998e59fb4
Sai Charan Reddy Patlolla <scrpatlolla@charan.home>
2017-01-01 17:10:05 -0500

    Added the data structure and function constructor

--- app.js
+++ app.js
@@ -2,9 +2,33 @@
 
 var budgetController = (function() {
 	
+	//Function Constructor
 	
+	var Expense = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 	
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 	
+	//Data Structure
+	
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp:0,
+			inc:0
+		}
+	}
+		
 })();
 
 
