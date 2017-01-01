72e8c13a45cc2953d16534b9cc0d1195613b1096
Sai Charan Reddy Patlolla <scrpatlolla@charan.home>
2017-01-01 15:48:55 -0500

    DRY

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Budget Controller
+
 var budgetController = (function() {
 	
 	
@@ -5,6 +7,8 @@ var budgetController = (function() {
 	
 })();
 
+
+//UI Controller
 
 var UIController = (function() {
 
@@ -14,9 +18,75 @@ var UIController = (function() {
 })();
 
 
-var controller = (function(budgetCtrl, UICtrl){
+//Global App Controller
 
+var controller = (function(budgetCtrl, UICtrl){
+	
+	//As Part of DRY
+	
+	var ctrlAddItem = function() {
+		
+		
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', function() {
+		
+		//1. Get the field Input Data
+		
+		
+		
+		
+		
+		//2. Add the Item to the Budget Controller
+		
+		
+		
+		
+		
+		//3. Add the item to the UI
+		
+		
+		
+		
+		
+		//4.Calculate the budget
+		
+		
+		
+		
+		
+		//5.Display the Budget on the UI
+		
+		
+		
+		
+	});
+	
+	//When you press Enter or Return key
+	
+	document.addEventListener('keypress', function(event) {
+		
+		if(event.keyCode === 13 || event.which === 13) {
+			console.log("ENTER was pressed.");
+		}
+		
+	});
 	
 	
 	
 })(budgetController, UIController);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
